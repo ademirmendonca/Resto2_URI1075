9d69fc62515957f992ca4e9993dd25305398eff1
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-17 10:21:03 -0300

    Add project files.

---
+++ Resto2_URI1075/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Resto2_URI1075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Efetua a leitura do número informado
+            int n = int.Parse(Console.ReadLine());
+                        
+            for (int i = 0; i <= 10000; i++)
+            {
+                int resto = 0;
+                
+                //Identifica os valores dos restos que são iguais a "2".
+                resto = i % n;
+
+                //Imprime apenas os números que na divisão, são iguais a zero.
+                if (resto ==2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
